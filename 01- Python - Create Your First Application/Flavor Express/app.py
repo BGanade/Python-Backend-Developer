0d17911bec950acba4a3d2f7e2b9e19698b4f067
Bruno Ganade Alves <bganade@gmail.com>
2026-06-27 16:08:39 -0300

    fix: handle invalid user input with try-except

--- 01- Python - Create Your First Application/Flavor Express/app.py
+++ 01- Python - Create Your First Application/Flavor Express/app.py
@@ -19,23 +19,33 @@ def ending_application(): #End the application
     os.system('cls')
     print('Ending the application\n')  
 
-def chose_option(): #Choose an option
-    chosed_option = int(input('Chose an option: '))
-    print(f'You chose option: {chosed_option}!')  
+def invalid_option(): #Show an invalid option message
+    print('Invalid option!\n')
+    input('Press enter to continue...')
+    main()
 
-    match chosed_option:
-        case 1:
-            print('Register a restaurant')
-        case 2:
-            print('List restaurants')
-        case 3:
-            print('Active restaurants')
-        case 4:
-            ending_application()
-        case _:
-            print('Invalid option')
+def chose_option(): #Choose an option
+    try:
+        chosed_option = int(input('Chose an option: '))
+        print(f'You chose option: {chosed_option}!')  
+
+        match chosed_option:
+            case 1:
+                print('Register a restaurant')
+            case 2:
+                print('List restaurants')
+            case 3:
+                print('Active restaurants')
+            case 4:
+                ending_application()
+            case _:
+                invalid_option()
+    except ValueError:
+        invalid_option()
+        return 
 
 def main():
+    os.system('cls')
     show_program_name()
     show_options()
     chose_option()
