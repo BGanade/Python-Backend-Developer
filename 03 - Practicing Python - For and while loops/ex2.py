71990fcb4fd28b67b7da367c0f903521f5fa86a8
BGanade <bganade@gmail.com>
2026-07-02 15:58:22 -0300

    create ex2.py fron modul 3 of the base

---
+++ 03 - Practicing Python - For and while loops/ex2.py
@@ -0,0 +1,10 @@
+""" Marcos is developing a program to repeatedly display a welcome message in the 
+console as part of a marketing campaign for his platform, Buscante. He wants to ensure 
+the message is displayed 5 times.
+
+Help Marcos write a program that displays the message "Welcome to Buscante!" the exact
+ number of times he needs. """
+
+
+for i in range(5):
+    print('Welcome to Buscante!')
