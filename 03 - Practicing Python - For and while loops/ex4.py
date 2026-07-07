814f0647b47dd2b24061a2160bd86bc2758bdb39
Bruno Ganade Alves <bganade@gmail.com>
2026-07-07 20:56:22 -0300

    feat: add module 3 exercise 4
    Added the fourth exercise for the Base level Module 3 as a separate Python file.

---
+++ 03 - Practicing Python - For and while loops/ex4.py
@@ -0,0 +1,19 @@
+""" Ana is developing a portfolio to showcase the Python projects she has completed. 
+She has organized a list containing the name of each project but noticed that some items
+ might be missing, appearing as `None`:
+
+
+Copy
+projects = ["website", "game", "data analysis", None, "mobile app"]
+
+Create a program that helps Ana iterate through the list of projects and displays the 
+names of the valid projects. If it encounters a `None` item, the program should display 
+the message: "Missing project". """
+
+projects = ["website", "game", "data analysis", None, "mobile app"]
+
+for project in projects:
+    if project != None:
+        print(project)
+    else:
+        print("Missing Project")
