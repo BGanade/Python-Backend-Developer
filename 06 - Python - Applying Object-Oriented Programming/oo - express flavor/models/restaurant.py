68d00a552f3c31e8c9f0cb2ff5ed670231846e31
Bruno Ganade Alves <bganade@gmail.com>
2026-08-31 23:06:34 -0300

    Update restaurant

--- 06 - Python - Applying Object-Oriented Programming/oo - express flavor/models/restaurant.py
+++ 06 - Python - Applying Object-Oriented Programming/oo - express flavor/models/restaurant.py
@@ -1,13 +1,13 @@
 class Restaurant:
     restaurants = []
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        self._name = name.title()
+        self.category = category.upper()
         self._active = False
         Restaurant.restaurants.append(self)
 
     def __str__(self):
-        return f'{self.name} | {self.category}'
+        return f'{self._name} | {self.category}'
 
     def list_restaurants():
         print(f'{'Restaurant Name'.ljust(25)} |'
@@ -15,7 +15,7 @@ class Restaurant:
               f'{'Status'}')
         for restaurant in Restaurant.restaurants:
             print(
-                f'{restaurant.name.ljust(25)} |'
+                f'{restaurant._name.ljust(25)} |'
                 f'{restaurant.category.ljust(25)} |'
                 f'{restaurant.active}'
             )
@@ -24,7 +24,8 @@ class Restaurant:
     def active(self):
         return 'active' if self._active else 'inactive'
 
-restaurant_praca = Restaurant('Praça', 'Gourmet')
-restaurant_pizza = Restaurant('Pizza Express', 'Italiano')
+restaurant_praca = Restaurant('praça', 'Gourmet')
+restaurant_praca._name = 'praça 2.0'
+restaurant_pizza = Restaurant('pizza express', 'Italiano')
 
 Restaurant.list_restaurants()
