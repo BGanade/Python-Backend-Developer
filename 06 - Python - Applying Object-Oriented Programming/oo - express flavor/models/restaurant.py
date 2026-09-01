cf8d4899b392888d38108505eb6966b4bea2cbf5
BGanade <bganade@gmail.com>
2026-09-01 14:37:03 -0300

    Create a function to alternate the status of the restaurants

--- 06 - Python - Applying Object-Oriented Programming/oo - express flavor/models/restaurant.py
+++ 06 - Python - Applying Object-Oriented Programming/oo - express flavor/models/restaurant.py
@@ -1,5 +1,6 @@
 class Restaurant:
     restaurants = []
+
     def __init__(self, name, category):
         self._name = name.title()
         self.category = category.upper()
@@ -9,11 +10,12 @@ class Restaurant:
     def __str__(self):
         return f'{self._name} | {self.category}'
 
-    def list_restaurants():
+    @classmethod
+    def list_restaurants(cls):
         print(f'{'Restaurant Name'.ljust(25)} |'
-              f'{'Category'.ljust(25)} |' 
+              f'{'Category'.ljust(25)} |'
               f'{'Status'}')
-        for restaurant in Restaurant.restaurants:
+        for restaurant in cls.restaurants:
             print(
                 f'{restaurant._name.ljust(25)} |'
                 f'{restaurant.category.ljust(25)} |'
@@ -24,8 +26,12 @@ class Restaurant:
     def active(self):
         return 'active' if self._active else 'inactive'
 
+    def Alternate_status(self):
+        self._active = not self._active
+
+
 restaurant_praca = Restaurant('praça', 'Gourmet')
-restaurant_praca._name = 'praça 2.0'
+restaurant_praca.Alternate_status()
 restaurant_pizza = Restaurant('pizza express', 'Italiano')
 
 Restaurant.list_restaurants()
