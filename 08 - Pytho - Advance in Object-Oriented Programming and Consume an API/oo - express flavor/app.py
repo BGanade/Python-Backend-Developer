3e649ad2ec103144c9dee35d7b57aa96af21542f
BGanade <bganade@gmail.com>
2026-09-09 12:04:48 -0300

    feat: add apply_discount method to Dish and Drinks classes
    Implemented the apply_discount method in the Dish and Drinks classes, allowing each class to apply its own discount logic.

--- 08 - Pytho - Advance in Object-Oriented Programming and Consume an API/oo - express flavor/app.py
+++ 08 - Pytho - Advance in Object-Oriented Programming and Consume an API/oo - express flavor/app.py
@@ -4,7 +4,9 @@ from models.menu.dish import Dish
 
 restaurant_square = Restaurant('square', 'gourmet')
 juice_drink = Drinks('Watermelon', 5.0, '500ml')
+juice_drink.apply_discount()
 bread_dish = Dish('Bread', 2.00, 'The best Bread of the city')
+bread_dish.apply_discount()
 restaurant_square.add_to_menu(juice_drink)
 restaurant_square.add_to_menu(bread_dish)
 
